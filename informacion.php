fe0b192e5d35e8773a3be85a85fddeb2ddcfa225
fgarcia-snna <3181675.garcia.fabian@gmail.com>
2026-08-15 08:54:42 -0500

    Create informacion.php

---
+++ informacion.php
@@ -0,0 +1,10 @@
+<?php
+$identificacion= $_post ['identificacion'];
+$NombreApellidos= $_POST ['NombreApellidos'];
+$cargo= $_post['cargo'];
+$salarios= $_post['salario'];
+echo "La identificacion del empleado es: ".Identificacion;
+echo "Los nombres y apellidos del empleado son:".NombreApellidos;
+echo "El cargo del empleado es:" .Cargo;
+echo "El salario del empleado es:" .Salario;
+?>
